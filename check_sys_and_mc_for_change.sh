a1fd215280c0a3d18802918472af239b11e9fea6
John Steill <jsteill@morgridge.org>
2019-07-04 06:23:41 -0500

    July .bash check - openjdk & cactus

--- check_sys_and_mc_for_change.sh
+++ check_sys_and_mc_for_change.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd ~/bin/Bash_Profile
-diff ReferenceProfiles/isitools_bash_profile /isitools/.bash_profile
+diff ReferenceProfiles/isitools_mirror/.bash_profile /isitools/.bash_profile
+printf "\n --------     ISITOOLS  -------- \n\n"
+ls -lart /isitools/.bash_*
+
+printf "\n --------     MC  -------- \n\n"
+ls -lart ~mikec/.bash_*
 
