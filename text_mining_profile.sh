b816adc7f1a169b2a07f8f5b5dd30d25889d2b11
John Steill <jsteill@morgridge.org>
2020-07-17 11:44:18 -0500

    JULY_20 added new .bash*

---
+++ text_mining_profile.sh
@@ -0,0 +1,5 @@
+source /w4home/jsteill/bin/Python_Virtual_Envs/TM_372/bin/activate
+export tm_user=dduser
+export tm_pass=searchtime
+export query_archive_db='/tmp/qry_archv.db'
+export PYTHONPATH=/isitools/Lbd_Stew:$PYTHONPATH
